9a2ce87850e9ce734093b54306e296917c6dc602
Zahra Sadeghi <thisiszahrasadeghi@gmail.com>
2024-05-26 15:49:26 +0330

    Update Game.py

--- Game.py
+++ Game.py
@@ -31,42 +31,47 @@ scissors = r'''
 ---.__(___)
                 '''
 
-
-user_choice = int(input("what do you choose? Type 0 for \"Rock\" , 1 for \"Paper\" and 2 for \"Scissors\" ."))
-game_images = [rock , paper , scissors ]
-
-if user_choice <= 2:
+continue_game = True
+while continue_game :
+    user_choice = int(input("what do you choose? Type 0 for \"Rock\" , 1 for \"Paper\" and 2 for \"Scissors\" ."))
+    game_images = [rock , paper , scissors ]
     
-    print(game_images[user_choice])
+    if user_choice <= 2:
+        
+        print(game_images[user_choice])
+        
+        computer_choice = random.randint(0,2)
+        print("Computer Choice :")
+        print(game_images[computer_choice])
     
-    computer_choice = random.randint(0,2)
-    print("Computer Choice :")
-    print(game_images[computer_choice])
-
-
-    if user_choice == 0:
-        if computer_choice == 0 :
-            print("TRY AGAIN")
-        elif computer_choice == 1 :
-            print("YOU LOSE")
-        else :
-            print("YOU WIN")
     
-    if user_choice == 1:
-        if computer_choice == 0 :
-            print("YOU WIN")
-        elif computer_choice == 1 :
-            print("TRY AGAIN")
-        else :
-            print("YOU LOSE")
+        if user_choice == 0:
+            if computer_choice == 0 :
+                print("TRY AGAIN")
+            elif computer_choice == 1 :
+                print("YOU LOSE")
+            else :
+                print("YOU WIN")
+        
+        if user_choice == 1:
+            if computer_choice == 0 :
+                print("YOU WIN")
+            elif computer_choice == 1 :
+                print("TRY AGAIN")
+            else :
+                print("YOU LOSE")
+        
+        if user_choice == 2:
+            if computer_choice == 0 :
+                print("YOU LOSE")
+            elif computer_choice == 1 :
+                print("YOU WIN")
+            else :
+                print("TRY AGAIN")
     
-    if user_choice == 2:
-        if computer_choice == 0 :
-            print("YOU LOSE")
-        elif computer_choice == 1 :
-            print("YOU WIN")
-        else :
-            print("TRY AGAIN")
+    else :
+        print("Please choose 0 , 1 or 2.")
 
-else :
-    print("Please choose 0 , 1 or 2.")
+    temp = str(input("Do you want to try again? Yes or No : ")).lower()
+    if temp == "no" :
+        continue_game = False
